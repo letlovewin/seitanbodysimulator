28c905558cdfd713434ac4b5eda383a009c52e42
sav1on <savionmajor@gmail.com>
2024-11-15 12:23:50 -0800

    Poop

--- src/main.py
+++ src/main.py
@@ -11,7 +11,67 @@ import time
 
 root = ThemedTk() # The actual window that'll pop up when you start the program.
 root.title("Seitan N-Body Simulator")
-root.resizable(False,False)
+root.resizable(True,True)
+
+root_canvas = Canvas(root,height=1000,width=1000, bg="#000000") # The "turtle" will draw onto this canvas here. It's black as indicated by the h
+
+universe = gravity.Universe(radius=150)
+
+#Beginning of aesthetics
+
+def on_button_click_start():
+    root.mainloop() #! root.mainloop() or some sort of pause? Has inverted effect, will stop if clicked & otherway around
+def on_button_click_stop():
+    root.quit()
+
+#Frame for left side UI elements
+left_frame = Frame(root, width = 300, height = 200, bg = "#e5e2e2")
+left_frame.pack(side = "left", fill="y")
+
+#Buttons for start & stop of program
+start_button = Button(left_frame, text= "Start Simulation",font=("Arial",14), borderwidth=5, relief="solid",padx=20,pady=10, command =on_button_click_start)
+start_button.pack(fill="x")
+
+stop_button = Button(left_frame,text="Stop Simultation",font=("Arial",14),borderwidth=5, relief="solid",padx=20,pady=10, command = on_button_click_stop)
+stop_button.pack(fill="x")
+
+#! Question of whether or not to create another particle class or import from gravity.py file?
+#!  --- > See notes - savi
+
+#Entry widgets for particle attributes
+label_name = Label(left_frame, text = "Particle Name: ")
+label_name.pack(fill="x")
+
+entry_name = Entry(left_frame)
+entry_name.pack(fill="x")
+
+label_x = Label(left_frame, text = "X Position: ")
+label_x.pack(fill="x")
+entry_x = Entry(left_frame)
+entry_x.pack(fill="x")
+
+label_y = Label(left_frame, text = "Y Position: ")
+label_y.pack(fill="x")
+entry_y = Entry(left_frame)
+entry_y.pack(fil ="x")
+
+label_x_velocity = Label(left_frame, text = "X Velocity: ")
+label_x_velocity.pack(fill="x")
+entry_x_velocity = Entry(left_frame)
+entry_x_velocity.pack(fill="x")
+
+label_y_velocity = Label(left_frame, text = "Y Velocity: ")
+label_y_velocity.pack(fill="x")
+entry_y_velocity = Entry(left_frame)
+entry_y_velocity.pack(fill="x")
+
+label_mass = Label(left_frame, text = "Mass: ")
+label_mass.pack(fill="x")
+entry_mass = Entry(left_frame)
+entry_mass.pack(fill="x")
+
+button_create = Button(left_frame, text = "Add Particle", command=lambda : gravity.Particle(universe,root_canvas,[float(entry_x.get()),float(entry_y.get())],[float(entry_x_velocity.get(),entry_y_velocity.get())],entry_mass.get(),entry_name.get())) # ! Insert command here
+button_create.pack(fill= "x")
 
 root_canvas = Canvas(root,height=1000,width=1000) # The "turtle" will draw onto this canvas here. It's black as indicated by the hex.
 
@@ -23,7 +83,7 @@ delta_t = 86400
 a = 0
 b = 86400 * 10 * 365
 
-universe = gravity.Universe(radius=150)
+
 
 for i in range(4):
     px = random.uniform(-150,150)
